7ba9ce9cf9fab4c11aa1b4eb65e5ac38f4a8aed1
Vikas007Vikas <vikaskamineni7@gmail.com>
2023-04-28 16:55:22 -0700

    update image paths

--- src/components/gameboard.js
+++ src/components/gameboard.js
@@ -7,6 +7,13 @@ import {db} from '../base';
 import {collection, addDoc} from 'firebase/firestore'
 import NavBar from "./NavBar";
 import { decode } from "base-64";
+import image0 from "./images/0.jpg"
+import image1 from "./images/1.jpg"
+import image2 from "./images/2.jpg"
+import image3 from "./images/3.jpg"
+import image4 from "./images/4.jpg"
+import image5 from "./images/5.jpg"
+import image6 from "./images/6.jpg"
 
 const Gameboard = ({ maxGuesses, onGameOver }) => {
   const [word, setWord] = useState("");
@@ -23,7 +30,7 @@ const Gameboard = ({ maxGuesses, onGameOver }) => {
   const [score, setScore] = useState(0);
   const navigate = useNavigate();
 
-  const [hangmanImages, setHangmanImages] = useState([  "./images/0.jpg",  "./images/1.jpg",  "./images/2.jpg",  "./images/3.jpg",  "./images/4.jpg",  "./images/5.jpg",  "./images/6.jpg",]);
+  const [hangmanImages, setHangmanImages] = useState([image0, image1, image2, image3, image4, image5, image6]);
 
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
